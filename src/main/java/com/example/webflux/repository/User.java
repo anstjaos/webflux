6efbf0f85510d1e1330d10c34478655638f7a2c8
anstjaos <vv_eros@naver.com>
2023-11-19 12:49:29 +0900

    User repository 생성

---
+++ src/main/java/com/example/webflux/repository/User.java
@@ -0,0 +1,22 @@
+package com.example.webflux.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class User {
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
